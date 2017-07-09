685c5cb39a0e50fe72d07b9c6417fd91b4cc00c4
barsic2017 <marinka28.04@mail.ru>
2017-07-09 21:48:52 +0300

    Изменения в программе

--- sandbox/src/main/java/ru/stqa/barsic/sandbox/Myfirstprogr.java
+++ sandbox/src/main/java/ru/stqa/barsic/sandbox/Myfirstprogr.java
@@ -3,6 +3,22 @@ package ru.stqa.barsic.sandbox;
 public class Myfirstprogr {
 
 public static void main(String[] args) {
-System.out.println("Hello, world!");
+hello("world");
+    hello("user");
+    hello("barsic");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 3;
+    double b = 4;
+    System.out.println("Площать прямоугольника со сторонами " + a + " и " + b + " = " + area (a,b));
 }
+public static void hello(String somebody)
+{
+    System.out.println("Hello," + somebody + "!");}
+
+    public static double area (double l) {return l*1;}
+
+    public static double area (double a, double b) {return a*b;}
 }
